36d9acfacb89ced13c1d131980ab653042a7c2d5
GreenBeanZ002 <110616479+GreenBeanZ002@users.noreply.github.com>
2026-09-16 17:13:05 +0100

    Feat - started adding ghost enemy, chore - reorganised some files

--- Assets/Scripts/Classes/Monster/Ghost.cs
+++ Assets/Scripts/Classes/Monster/Ghost.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
-public class Skeleton : Monster
+public class Ghost : Monster 
 {
     [SerializeField] private Sprite monsterSprite;
 
     protected override void SetDefaults()
     {
-        MONSTER_NAME = "Skeleton";
-        MONSTER_HEALTH = 10;
-        MONSTER_SPEED = 5f;
-        MONSTER_DAMAGE = 10;
+        MONSTER_NAME = "Ghost";
+        MONSTER_HEALTH = 5;
+        MONSTER_SPEED = 7f;
+        MONSTER_DAMAGE = 0;
         MONSTER_SPRITE = monsterSprite;
     }
 }
+
